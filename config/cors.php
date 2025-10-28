ee62efb2919e97cd183cd4a97dbbb4b14b3e5dbc
Saza4 <allgamesmexofficial@gmail.com>
2025-10-28 15:14:52 -0600

    mejora: Cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,27 +2,24 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
 
     'allowed_methods' => ['*'],
 
     // Orígenes explícitos de Vite en local
     'allowed_origins' => [
-        'http://localhost:5173',    
+        'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5174',
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*', 'Authorization'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    // Para BEARER tokens no hace falta enviar cookies:
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
