25703d8cb8ef338b2ad6c741d30807aa3d06a7bd
erikflores16 <136302396+erikflores16@users.noreply.github.com>
2025-10-30 13:21:51 -0600

    Add new allowed origin for CORS
    Added a new allowed origin for CORS configuration.

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,24 @@
 <?php
 
- return [
+return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    
     'allowed_methods' => ['*'],
-
+    
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://mifront-6stl.onrender.com',
+        'https://mifront-1.onrender.com',  // ✅ Agrega esta línea
     ],
-
+    
     'allowed_origins_patterns' => [],
-
+    
     'allowed_headers' => ['*'],
-
+    
     'exposed_headers' => [],
-
+    
     'max_age' => 0,
-
+    
     'supports_credentials' => true,
 ];
