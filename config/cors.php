d4ace506995af7f5a1b04418035611b2f8b3d581
erikflores16 <erikfloresherrera290@gmail.com>
2025-10-30 08:28:37 -0600

    corss

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://mifront-6stl.onrender.com', 
     ],
 
     'allowed_origins_patterns' => [],
